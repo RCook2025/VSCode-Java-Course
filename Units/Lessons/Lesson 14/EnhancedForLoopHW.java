02640290c1718cc00fc815fbbb083752b390dab4
RCook2025 <147068426+RCook2025@users.noreply.github.com>
2023-12-13 09:52:13 -0500

    EnhancedForLoopHW
    This is for HW Submission 12/13

---
+++ Units/Lessons/Lesson 14/EnhancedForLoopHW.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class EnhancedForLoopHW {
+    public static void main(String[] args) {
+        double[] numbers = {2.2, 1.0, 3.7, 4.1, 88.0};
+        for (double element : numbers) {
+            System.out.println(element);
+        }
+    }
+}
